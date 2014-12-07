f2f54769d98c1928c3c34eaa241b84559f48939f
thekb <kbschandra@gmail.com>
2014-12-07 23:52:17 +0530

    removing service skills model

--- serviceprovider/models.py
+++ serviceprovider/models.py
@@ -1,11 +1,10 @@
 from sqlalchemy import Column, Integer, String, Boolean, Float, ForeignKey
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.dialects.postgres import ARRAY
+from sqlalchemy.dialects.postgres import ARRAY, JSON
 
 import db
 
 
-__all__ = ['Service', 'ServiceProvider', 'ServiceSkill']
+__all__ = ['Service', 'ServiceProvider']
 
 
 class Service(db.Base):
@@ -24,20 +23,11 @@ class ServiceProvider(db.Base):
     availability = Column("availability", Boolean, default=False)
     phone_number = Column("phone_number", String(20), nullable=False)
     address = Column("address", String(2048), nullable=False)
-    home_location = Column("home_location", ARRAY(Float), nullable=False)
-    office_location = Column("office_location", ARRAY(Float), nullable=False)
+    home_location = Column("home_location", ARRAY(Float, dimensions=1), nullable=False)
+    office_location = Column("office_location", ARRAY(Float, dimensions=1), nullable=False)
     cost_per_hour = Column("cost", Float, default=0.0)
     service = Column("service", ForeignKey('service.id'))
     experience = Column("experience", Float, default=0.0)
+    skills = Column("skills", ARRAY(JSON, dimensions=1))
     trash = Column("trash", Boolean, default=False)
-    skills = relationship("ServiceSkill", backref="service_provider")
 
-
-class ServiceSkill(db.Base):
-    __tablename__ = 'service_skill'
-
-    id = Column(Integer, primary_key=True)
-    name = Column("name", String(512), nullable=False)
-    inspection = Column("inspection", Boolean, default=False)
-    provider = Column("provider", ForeignKey('service_provider.id'))
-    trash = Column("trash", Boolean, default=False)
