9d1d2a5996d8d12370444718625eea3544e7c648
thekb <kbschandra@gmail.com>
2014-12-21 17:50:06 +0530

    updating service provider required fields

--- admin/handlers.py
+++ admin/handlers.py
@@ -89,8 +89,7 @@ def handle_exceptions(function):
 class ServiceProviderHandler(BaseHandler):
     resource_name = 'serviceprovider'
     create_required = {'name', 'phone_number', 'address', 'home_location',
-                       'office_location', 'cost', 'service', 'experience',
-                       'skills'}
+                       'office_location', 'cost', 'experience'}
     update_ignored = {'service'}
 
     @handle_exceptions
