84c862d3a8516a59f2a6a368cd9bbe8e3ce44507
Gautam BT <gautambt@gmail.com>
2015-02-07 00:05:44 +0530

    log exceptions

--- exc.py
+++ exc.py
@@ -1,5 +1,7 @@
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 from tornado.web import HTTPError
+import traceback
+import logging
 
 
 class AppException(Exception):
@@ -11,20 +13,24 @@ def handle_exceptions(function):
         try:
             return function(instance, *args, **kwargs)
         except AppException as ae:
+            logging.warn(traceback.format_exc())
             instance.set_status(400)
             instance.write({
                 'error': str(ae)
             })
             instance.flush()
         except (NoResultFound, MultipleResultsFound):
+            logging.warn(traceback.format_exc())
             instance.set_status(400)
             instance.write({
                 'error': 'No results found'
             })
             instance.flush()
         except HTTPError as he:
+            logging.warn(traceback.format_exc())
             raise he
         except Exception as e:
+            logging.warn(traceback.format_exc())
             instance.set_status(500)
             instance.write({
                 'error': str(e)
