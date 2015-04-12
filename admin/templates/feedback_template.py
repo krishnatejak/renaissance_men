54b1cfd9e6d9b84ffd35ae453345f8eed9f31ba2
krishtk <krishna@bubblewrapp.com>
2015-04-13 00:39:20 +0530

    rating order changed from 1-5 to 5-1

--- admin/templates/feedback_template.py
+++ admin/templates/feedback_template.py
@@ -4,11 +4,11 @@ feedback_template = """
 <p>Your {service} Order No. {order_id} is completed. </p>
 <p>Please rate your experience with Sevame.</p>
 <form action="{rating_link}" method="GET">
-    <input name="rating" type="radio" value="1"/> ★☆☆☆☆<br />
-    <input name="rating" type="radio" value="2"/> ★★☆☆☆<br />
-    <input name="rating" type="radio" value="3"/> ★★★☆☆<br />
-    <input name="rating" type="radio" value="4"/> ★★★★☆<br />
     <input name="rating" type="radio" value="5"/> ★★★★★<br />
+    <input name="rating" type="radio" value="4"/> ★★★★☆<br />
+    <input name="rating" type="radio" value="3"/> ★★★☆☆<br />
+    <input name="rating" type="radio" value="2"/> ★★☆☆☆<br />
+    <input name="rating" type="radio" value="1"/> ★☆☆☆☆<br />
     <input name="orderinfo" type="hidden" value="{order_identity}" />
     <p style="padding-left:2%">
         <input type="submit" value="Submit" style="background-color: #f0ad4e; height:27px; color:white; border-radius:2px;
